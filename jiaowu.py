d94266f66301592466c10ec851a5f75d525e043c
oyster <salt_oyster@outlook.com>
2019-10-07 16:48:32 +0800

    try

---
+++ jiaowu.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import time
+
+
+def getnextdate():
+	ticks = time.time()
+	print(ticks)
+
+def getdata(number,password,weeknumber):
+	login_url = "http://jwxt.scau.edu.cn/secService/login"
+	course_url = "http://jwxt.scau.edu.cn/resService/jwxtpt/v1/xsd/xsdqxxkb_info/searchOneXskbList?resourceCode=XSMH0701&apiCode=jw.xsd.xsdInfo.controller.XsdQxxkbController.searchOneXskbList"
+
+	payloadData ={"userCode":number,"password":password,"kaptcha":"testa","userCodeType":"account"}
+
+	'''登陆头'''
+	headers = {
+		'Accept': 'application/json, text/plain, */*',
+		'Accept-Encoding': 'gzip, deflate',
+        'app': 'PCWEB',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Host': 'jwxt.scau.edu.cn',
+        'KAPTCHA-KEY-GENERATOR-REDIS': 'securityKaptchaRedisServiceAdapter',
+        'Origin': 'http://jwxt.scau.edu.cn',
+        'Referer': 'http://jwxt.scau.edu.cn/Njw2017/login.html',
+        'X-Requested-With': 'XMLHttpRequest',
+        'cache-control': 'no-cache',  
+	}	
+
+
+	s = requests.session()
+	dumpJsonData   = json.dumps(payloadData)
+	login_response = s.post(login_url , headers = headers , data = dumpJsonData)
+	login_session = login_response.cookies.get_dict()['SESSION']	
+	'''print(login_session)'''
+	login_response = json.loads(login_response.text)
+	token = login_response['data']['token']
+
+	'''构造获取课表头'''
+	course_headers = {
+	'Accept': 'application/json, text/plain, */*',
+	'Accept-Encoding': 'gzip, deflate',
+	'Accept-Language': 'zh-CN,zh;q=0.9',
+	'app': 'PCWEB',
+	'Connection': 'keep-alive',
+	'Content-Length': '65',
+	'Content-Type': 'application/json',
+	'Host': 'jwxt.scau.edu.cn',
+	'locale': 'zh_CN',
+	'Origin': 'http://jwxt.scau.edu.cn',
+	'Referer': 'http://jwxt.scau.edu.cn/Njw2017/index.html',
+	'Simulated-By':'', 
+	'TOKEN': token,
+	'User-Agent': 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14',
+	'userAgent':'' ,
+	'userRoleCode': 'student',
+	'X-Requested-With': 'XMLHttpRequest'
+	}
+	course_headers['Cookie'] = f'SESSION={login_session}; token='
+	couser_payload = {"jczy013id":"2019-2020-1","pkgl002id":"W13414710000WH","zt":"2","pkzc": weeknumber}
+	dumpJsonData     = json.dumps(couser_payload)
+	course_response = s.post(course_url , headers = course_headers , data = dumpJsonData)
+	'''print(course_response.text)'''
+	ddd = json.loads(course_response.text)
+
+	localtime = time.localtime(time.time())
+	course = []
+	for i in ddd['data']:
+		
+		if(int(i['pksjmx'][0])==localtime[6]+2):
+			course.append(i)
+		'''print(i['pksjshow'],i['kc_name'])'''
+	for i in course:
+		print(i['pksjshow'],i['kc_name'])
+	return course 	
+
+
+
+if __name__ == "__main__":
+	'''学号密码以及第几周'''
+	number = ""
+	password = ""
+	week = "6"
+	data = getdata(number,password,week)
+	
